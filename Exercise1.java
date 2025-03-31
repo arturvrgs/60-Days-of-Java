03eab8b85ac125045e5654ffa811442d7f75a8f0
Artur V. Vargas <arturvargaslg@gmail.com>
2025-03-31 15:56:29 -0300

    add exercise1

---
+++ Exercise1.java
@@ -0,0 +1,21 @@
+// Importing the necessary Java package for standard input-output
+import java.util.Scanner;
+
+public class Exercise1 {
+    public void displayText() {
+        // Prompt the user to enter a message
+        System.out.println("Enter your message:");
+        // Use a Scanner to read the user's input
+        //TODO
+
+        // Print the message using println (with a newline)
+        //TODO
+
+        // Print the message using print (without a newline)
+        //TODO
+
+        // Print a new line with escape sequences
+        //TODO
+    }
+}
+
