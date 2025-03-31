ea2bdad6b3dabd51e244945aaba004cc88ed755b
Artur V. Vargas <arturvargaslg@gmail.com>
2025-03-31 19:25:41 -0300

    finished exercise1

--- Exercise1.java
+++ Exercise1.java
@@ -7,15 +7,20 @@ public class Exercise1 {
         System.out.println("Enter your message:");
         // Use a Scanner to read the user's input
         //TODO
-
+            Scanner scan = new Scanner(System.in);
+            String message = scan.nextLine();
         // Print the message using println (with a newline)
         //TODO
-
+            System.out.println("Using println:");
+            System.out.print(message);
         // Print the message using print (without a newline)
         //TODO
-
+            System.out.print("Using print:");
+            System.out.print(message);
         // Print a new line with escape sequences
         //TODO
+        System.out.println("Text with scape sequences:");
+        System.out.println("Hello,\"Java Masterclass\"\nstudents!\nWelcome to Day 1\nLet's get started!");
     }
 }
 
