909de0b2e37f5cd93fb412ac3bbf05376e69a199
Artur Vargas <arturvargaslg@gmail.com>
2025-04-02 16:23:09 -0300

    add caloriecounterpro

---
+++ CalorieCounterPro.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CalorieCounterPro {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<String> foodItems = new ArrayList<>();
+        ArrayList<Integer> calorieValues = new ArrayList<>();
+
+        int calorieCounter = 0;
+
+        while(true) {
+
+            System.out.println("Enter a food: ");
+            String foodItem = scan.nextLine();
+
+            System.out.println("Enter its calorie: ");
+            int calorieValue = scan.nextInt();
+
+            foodItems.add(foodItem);
+            calorieValues.add(calorieValue);
+            calorieCounter += calorieValue;
+
+            System.out.println("Do you want to continue?");
+            System.out.println("1 = Yes");
+            System.out.println("2 = No");
+
+            int choice = scan.nextInt();
+            scan.nextLine();
+
+            if(choice != 1) break;
+        }
+
+        System.out.println("Your Calorie Intake: ");
+
+        for(int i = 0; i < foodItems.size(); i++) {
+            System.out.printf("%d : %s - %d calories\n", i+1, foodItems.get(i), calorieValues.get(i));
+        }
+
+        System.out.println("Your Calorie Total Intake: " + calorieCounter);
+
+    }
+}
